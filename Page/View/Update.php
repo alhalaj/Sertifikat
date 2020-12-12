1d11bb365f482687ad352b04d67a8ebe57ace191
Aris Supriadi <semar.prg@gmail.com>
2020-12-12 11:36:10 +0700

    Update Update.php

--- Page/View/Update.php
+++ Page/View/Update.php
@@ -10,8 +10,7 @@ require '../Include/Connect/Connections.php';
     if($update){
             header("location:../View/Seminar.php");
         } else {
-            // echo 'Error : '.$insert->error." ".$query;
-            // echo 'Error : '.mysqli_error($con);
+            
         }
 } else if(isset($_POST['Submit1'])){
     $id = base64_decode($_POST["pes"]);
@@ -29,19 +28,13 @@ require '../Include/Connect/Connections.php';
     if (!empty($berkas)){
         if(in_array($ekstensi, $ekstensi_diperbolehkan) === true){
             if($ukuran < 1044070){		
-            // echo $file_tmp;
-                $query = "UPDATE pendaftaran_seminar SET `bukti_byr` = '$berkas', `status`= '$status' where id_pendaftaran ='$id' ";
-                // echo 'Query : '.$con;
-                // echo 'Error : '.mysqli_error($con);
-                // echo $berkas;
+                $query = "UPDATE pendaftaran_seminar SET `bukti_byr` = '$berkas', `status`= '$status' where id_pendaftaran ='$id' ";                
                 $update = mysqli_query($con,$query);
                 if($update){
                     move_uploaded_file($file_tmp, '../Include/Image/berkas/'.$berkas);
                     if(file_exists($path.$oldberkas)){
                         unlink($path.$oldberkas);
                     }
-                //     // return true;
-                //     echo 'FILE BERHASIL DI UPLOAD';
                 header("location:../View/Peserta.php");
                 }else{
                     echo 'GAGAL MENGUPLOAD GAMBAR';
@@ -50,45 +43,16 @@ require '../Include/Connect/Connections.php';
                 echo 'UKURAN FILE TERLALU BESAR';
             }
         }else{
-            echo 'EKSTENSI FILE YANG DI UPLOAD TIDAK DI PERBOLEHKAN';
-            // header("location:../View/Peserta.php");
+            echo 'EKSTENSI FILE YANG DI UPLOAD TIDAK DI PERBOLEHKAN';   
         }
     }else {
-        // $berkas = empty($berkas) ? "buktibayar":$berkas;
         $query ="UPDATE pendaftaran_seminar SET `status`= '$status' where id_pendaftaran ='$id'";
-    // echo $status;
         $update = mysqli_query($con,$query);
         if($update){
                 header("location:../View/Peserta.php");
             } else {
-                // echo 'Error : '.$insert->error." ".$query."<br>";
-                // echo 'Error : '.mysqli_error($con);
             }
-           /* if(!empty($berkas)){
-            if(in_array($ekstensi, $ekstensi_diperbolehkan) === true){
-                if($ukuran < 1044070){		
-                // echo $file_tmp;
-                    if($update){
-                        move_uploaded_file($file_tmp, '../Include/Image/berkas/'.$berkas);
-                        if(file_exists($path.$oldberkas)){
-                            unlink($path.$oldberkas);
-                        }else {
-
-                        }
-                    //     // return true;
-                    //     echo 'FILE BERHASIL DI UPLOAD';
-                    }else{
-                        echo 'GAGAL MENGUPLOAD GAMBAR';
-                    }
-                }else{
-                    echo 'UKURAN FILE TERLALU BESAR';
-                }
-            }else{
-                header("location:../View/Peserta.php");
-            }
-        }else{
-            header("location:../View/Peserta.php");
-        }*/
+           
       }
 }
 ?>
