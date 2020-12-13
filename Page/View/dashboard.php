cf31f45f4d6b355a046eff90f490ab3e5c08bdc9
codeaflaha <dwiajakokrepot@gmail.com>
2020-12-13 18:59:14 +0700

    tambah halaman login

---
+++ Page/View/dashboard.php
@@ -0,0 +1,73 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php 
+        require '../Include/Header.php';
+        require '../Include/Connect/Connections.php';
+		$total = "select count(*) as JUM from peserta ";
+		$mhs = mysqli_query($con,$total);
+		$totalp = mysqli_fetch_array($mhs,MYSQLI_ASSOC);
+		$sql = "select count(*) as JUM2 from seminar ";
+		$seminar = mysqli_query($con,$sql);
+		$totals = mysqli_fetch_array($seminar,MYSQLI_ASSOC);
+    ?>
+</head>
+
+<body class="">
+  <div class="wrapper ">
+    <?php require '../Include/SideBar.php'; ?>  
+    <div class="main-panel">
+     <?php require '../Include/NavPanel.php';?>
+      <!-- End Navbar -->
+      <div class="content">
+        <div class="container-fluid">
+          <div class="row">
+		  
+		  <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="card card-stats">
+                                <div class="card-header" data-background-color="rose">
+                                    <i class="material-icons">equalizer</i>
+                                </div>
+                                <div class="card-content">
+                                    <p class="category">Peserta Seminar</p>
+                                    <h3 class="card-title"><?php echo $totalp['JUM'];?></h3>
+                                </div>
+                                <div class="card-footer">
+                                    <div class="stats">
+                                        <i class="material-icons">date_range</i> Last 24 Hours
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6">
+                            <div class="card card-stats">
+                                <div class="card-header" data-background-color="green">
+                                    <i class="material-icons">store</i>
+                                </div>
+                                <div class="card-content">
+                                    <p class="category">Jumlah Seminar</p>
+                                    <h3 class="card-title"><?php echo $totalp['JUM'];?></h3>
+                                </div>
+                                <div class="card-footer">
+                                    <div class="stats">
+                                        <i class="material-icons">date_range</i> Last 24 Hours
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+            
+            
+          </div>
+          
+          
+        </div>
+      </div>
+       <?php include_once '../Include/Footer.php'; ?>
+    </div>
+  </div>
+  <?php require '../Include/Js.php'; ?>
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="login/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="login/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Cetak Sertifikat
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <!-- CSS Files -->
+  <link href="login/css/material-kit.css?v=2.0.7" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="login/demo/demo.css" rel="stylesheet" />
+</head>
+
+<body class="landing-page sidebar-collapse">
+  <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="https://demos.creative-tim.com/material-kit/index.html">
+          Welcome </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+         
+         
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank" data-original-title="Follow us on Twitter" rel="nofollow">
+              <i class="fa fa-twitter"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.facebook.com/CreativeTim" target="_blank" data-original-title="Like us on Facebook" rel="nofollow">
+              <i class="fa fa-facebook-square"></i>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank" data-original-title="Follow us on Instagram" rel="nofollow">
+              <i class="fa fa-instagram"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="page-header header-filter" data-parallax="true" style="background-image: url('login/img/home.jpg')">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h1 class="title">Cetak Sertifikat Seminar</h1>
+          <h4>Silahkan Login / registrasi dahulu</h4>
+          <br>          
+		  <a href="http://pendaftaran.imla.or.id/imla_daftar_akun.php" target="_blank" class="btn btn-info btn-raised btn-lg">
+            <i class="material-icons">assignment</i> Daftar
+          </a>
+          <a href="http://pendaftaran.imla.or.id/imla_login.php" target="_blank" class="btn btn-success btn-raised btn-lg">
+            <i class="material-icons">assignment_ind</i> Masuk
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  
+  <!--   Core JS Files   -->
+  <script src="login/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="login/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="login/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+  <script src="login/js/plugins/moment.min.js"></script>
+  <!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+  <script src="login/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="login/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <!--  Google Maps Plugin    -->
+  <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
+  <script src="login/js/material-kit.js?v=2.0.7" type="text/javascript"></script>
+</body>
+
+</html>
